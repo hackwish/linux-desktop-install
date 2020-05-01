1ac0ec40e4d97a13525d7cca089e3017bc28529b
Marcelo Cardenas <marcelo.cardenasc@gmail.com>
2020-05-01 03:08:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,13 +27,31 @@ apt-get install -y curl \
 		rsync \
 		git \
 		software-properties-common \
-		apt-transport-https
-
-apt-add-repository --yes ppa:ansible/ansible
-
-DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ansible
+		apt-transport-https \
+		python2-minimal
 
 #Ansible
+echo "Instalando Ansible"
+#wget http://mirrors.kernel.org/ubuntu/pool/universe/p/paramiko/python-paramiko_2.6.0-1_all.deb
+#dpkg -i python-paramiko_2.6.0-1_all.deb
+
+alias pip='pip3'
+alias python='python3'
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install ansible
+
+# apt-add-repository --yes ppa:ansible/ansible
+
+#touch /etc/apt/sources.list.d/ansible-ubuntu-ansible-eoan.list
+#echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu eoan main" >> /etc/apt/sources.list.d/ansible-ubuntu-ansible-eoan.list
+# echo "deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu eoan main" >> /etc/apt/sources.list.d/ansible-ubuntu-ansible-eoan.list
+
+#apt-get update
+
+#DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ansible
+
 echo "Iniciando Ansible Deploy"
 ANSIBLE_CUSTOM_DIR=`pwd`
 
