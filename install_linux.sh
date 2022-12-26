e5aa01b119f446cccec44a38dd3c2f8a19e89dcc
Marcelo Cardenas (GLOBANT) <marcelocardenas.globant@latam.com>
2022-12-26 18:43:08 -0300

    fix(installer): add (mint) vera on jammy codename condition

--- install_linux.sh
+++ install_linux.sh
@@ -71,7 +71,7 @@ echo "Ahora vamos a adecuar la instalación..."
 # Fix Ubuntu codenames for based distros
 echo "CodeName ANTES: $DISTRIB_CODENAME"
 # Linux Mint distros
-if [ ${DISTRIB_CODENAME} == 'vanessa' ]; then
+if [ ${DISTRIB_CODENAME} == 'vanessa' ] || [ ${DISTRIB_CODENAME} == 'vera' ]; then
         echo $DISTRIB_CODENAME
         export DISTRIB_CODENAME='jammy'
 elif [ ${DISTRIB_CODENAME} == 'ulyana' ] || [ ${DISTRIB_CODENAME} == 'ulyssa' ] || [ ${DISTRIB_CODENAME} == 'uma' ] || [ ${DISTRIB_CODENAME} == 'una' ]; then
